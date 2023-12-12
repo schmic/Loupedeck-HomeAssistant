b646ff82ed1197ee1c6440e96a95feb755993566
Michael Scherer <me@schmic.eu>
2023-12-12 14:57:28 +0100

    prepare v0.6

--- package.ps1
+++ package.ps1
@@ -1,1 +1,1 @@
-LoupedeckPluginTool.exe pack -input=".\HaPlugin\bin\" -output="..\HomeAssistant-0.5.lplug4"
+LoupedeckPluginTool.exe pack -input=".\HaPlugin\bin\" -output="..\HomeAssistant-0.6.lplug4"
