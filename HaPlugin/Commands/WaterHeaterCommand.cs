18a4aac19be9be6bf247e3c2618c2c8f76d02539
Michael Scherer <me@schmic.eu>
2023-12-20 17:06:48 +0100

    add support for water_heater

---
+++ HaPlugin/Commands/WaterHeaterCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Loupedeck.HomeAssistant.Commands
+{
+    using System;
+
+    public class WaterHeaterCommand : BaseCommand
+    {
+        public WaterHeaterCommand() : base("Sensor") { }
+
+        protected override Boolean EntitiyFilter(String entity_id)
+            => entity_id.StartsWith("water_heater.");
+
+        protected override void RunCommand(String entity_id) { }
+    }
+}
