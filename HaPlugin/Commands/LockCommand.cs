c870dd529d3978652bbc0b4009db19ba448400e4
Michael Scherer <me@schmic.eu>
2023-12-20 17:06:11 +0100

    add support for lock entities

---
+++ HaPlugin/Commands/LockCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace Loupedeck.HomeAssistant.Commands
+{
+    using System;
+
+    using Newtonsoft.Json.Linq;
+
+    public class LockCommand : DualStateCommand
+    {
+        // my son decided that the offState is locked :)
+        public LockCommand() : base("Lock", offState: "locked", onState: "unlocked") { }
+
+        protected override Boolean EntitiyFilter(String entity_id)
+            => entity_id.StartsWith("lock.");
+
+        protected override void RunCommand(String entity_id)
+        {
+            var service = this.GetCurrentState(entity_id).Name.Equals(this.OffState) ? "unlock" : "lock";
+            var data = new JObject {
+                { "domain", "lock" },
+                { "service", service },
+                { "target", new JObject { { "entity_id", entity_id } } }
+            };
+
+            this.GetPlugin().CallService(data);
+        }
+    }
+}
