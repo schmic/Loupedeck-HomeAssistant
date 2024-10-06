82a73d50822a6c56cb6aba316ed18462296020bd
Michael Scherer <me@schmic.eu>
2024-10-06 10:33:17 +0200

    Prepare next release 0.9

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-LoupedeckPluginTool.exe pack -input="build\bin\Release" -output="..\HomeAssistant-0.8.lplug4"
+LoupedeckPluginTool.exe pack -input="build\bin\Release" -output="..\HomeAssistant-0.9.lplug4"
