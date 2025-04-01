34fb871168f0f5d539b93aeecb711a1226ebcdf3
Michael Scherer <me@schmic.eu>
2025-04-01 14:01:23 +0200

    Prepare v0.9

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-LoupedeckPluginTool.exe pack -input="build\bin\Release" -output="..\HomeAssistant-0.9.lplug4"
+LogiPluginTool.exe pack -input="build\bin\Release" -output="..\HomeAssistant-0.9.lplug4"
